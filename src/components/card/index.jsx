19c4c65382c9514b27c55cf2baac3fc21253b3e5
carolina <arlinb@gmail.com>
2020-08-07 13:00:29 -0300

    se empezo card

---
+++ src/components/card/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="card-container">
+      <div className="card-image-container">
+        <img
+          className="card-image"
+          src="https://image.tmdb.org/t/p/w370_and_h556_bestv2/kjMbDciooTbJPofVXgAoFjfX8Of.jpg
+"
+          alt="greyhound"
+        />
+      </div>
+
+      <h1 className="card-title">hola</h1>
+    </div>
+  );
+};
+export default Card;
